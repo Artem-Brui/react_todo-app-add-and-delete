cc3aef428269f2ecfc3638e45e4adbc96b11a9ad
Artem-Brui <artembryj@gmail.com>
2025-03-13 13:54:57 +0100

    transfer code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,85 @@
-/* eslint-disable max-len */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import { getTodos, USER_ID } from './api/todos';
+import { Todo } from './types/Todo';
+import TodoList from './components/TodoList/TodoList';
+import Footer from './components/Footer/Footer';
+import ErrorMessage from './components/ErrorMessage';
+import { ErrorType } from './components/ErrorMessage/types';
+import { getFiltredTodoList } from './components/Footer/service';
+import { Filter } from './components/Footer/types';
 
 export const App: React.FC = () => {
+  const [filter, setFilter] = useState<Filter>('FilterLinkAll');
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filtredTodos, setFiltredTodos] = useState<Todo[]>(
+    getFiltredTodoList(filter, todos),
+  );
+
+  const [error, setError] = useState<ErrorType>({
+    isVisible: false,
+    type: '',
+  });
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(() => {
+        setError({
+          isVisible: true,
+          type: 'load',
+        });
+
+        setTimeout(() => {
+          setError({
+            isVisible: false,
+            type: '',
+          });
+        }, 3000);
+      });
+  }, []);
+
+  useEffect(() => {
+    setFiltredTodos(getFiltredTodoList(filter, todos));
+  }, [filter, todos]);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <header className="todoapp__header">
+          {/* this button should have `active` class only if all todos are completed */}
+          <button
+            type="button"
+            className="todoapp__toggle-all active"
+            data-cy="ToggleAllButton"
+          />
+
+          {/* Add a todo on form submit */}
+          <form>
+            <input
+              data-cy="NewTodoField"
+              type="text"
+              className="todoapp__new-todo"
+              placeholder="What needs to be done?"
+            />
+          </form>
+        </header>
+
+        <TodoList todos={filtredTodos} />
+
+        {!!todos.length && (
+          <Footer todos={todos} filter={filter} updateFilter={setFilter} />
+        )}
+      </div>
+
+      <ErrorMessage error={error} />
+    </div>
   );
 };
